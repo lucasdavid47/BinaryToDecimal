5f7f3893b5258d38c037a25ada788aa23e218b83
Lucas david <lucas.david44000@gmail.com>
2020-05-05 14:48:06 +0200

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static Program instance;
+        
+        static void Main(string[] args)
+        {
+            instance = new Program();
+            
+            Console.WriteLine("Please enter the binary code to translate ! ");
+            String str = Console.In.ReadLine();
+            
+            Console.WriteLine(Program.instance.fromBinaryToDecimal(int.Parse(str)));
+        }
+
+
+        public int fromBinaryToDecimal(int binary)
+        {
+            return Convert.ToInt32(binary.ToString(), 2);
+            
+        }
+        
+    }
+}
+    
